b72c7bee63f3105652d266814d0678b42d6fe1ab
github_username_here <email@example.com>
2025-03-23 21:34:41 -0400

    edited code

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-with open("books/frankenstein.txt") as f:
-	file_contents = f.read()
-	return file_contents
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    print(text)
+
+main()
 
