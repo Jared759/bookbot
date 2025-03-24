f6071b5f3f61cfa1d8553ffa2464577867a11ab1
github_username_here <email@example.com>
2025-03-24 09:26:29 -0400

    edited comm

--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def numwords(path):
     words = len(path.split())
     return words
+
+def numletters(path):
+    dict = {}
+    count = 0
+    for i in path:
+        count += 1
+        dict =(f"{i}: {count}")
+    return dict
