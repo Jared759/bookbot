2e360c23302b4413bfcd0ca344c0d013b87f0402
github_username_here <email@example.com>
2025-03-24 10:30:14 -0400

    co ed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     text = get_book_text(path)
     word_count = numwords(text)
     letter_count = numcharacters(text)  # This should return the letter frequency dictionary
-    sorted_letters = sorted(letter_count)
+    sorted_letters = sort(letter_count)
     print(f"Found {word_count} total words.")
     print(sorted_letters)
 main()
