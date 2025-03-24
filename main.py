e8738af84093765f65067f4f1a556fbbab674b8f
github_username_here <email@example.com>
2025-03-24 10:50:37 -0400

    co ed

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ def get_book_text(path):
         return file_contents
 
 def main():
-    text = get_book_text(path)
-    word_count = numwords(text)
-    letter_count = numcharacters(text)  # This should return the letter frequency dictionary
-    sorted_letters = sort(letter_count)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    for item in sorted_letters:  # Loop through the sorted list
-        print(f"{item['letter']}: {item['count']}")
-
-    if len(sys.argv) != 2:
+      if len(sys.argv) != 2:
         print(f"Usage: python3 main.py books/")
         sys.exit(1)
     
-    book_path = sys.argv(1)
+book_path = sys.argv(1)
+text = get_book_text(book_path)
+word_count = numwords(text)
+letter_count = numcharacters(text)  # This should return the letter frequency dictionary
+sorted_letters = sort(letter_count)
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+for item in sorted_letters:  # Loop through the sorted list
+    print(f"{item['letter']}: {item['count']}")
+
 main()
 
 
