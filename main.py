d830b598f08899f9e5373a9bba33e327f0a5adb3
github_username_here <email@example.com>
2025-03-24 09:30:53 -0400

    edited code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import numwords
+from stats import numletters
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,8 +10,9 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = numwords(text)
+    letter_count = numletters(text)
     print(f"{word_count} words found in the document")
-
+    return letter_count
 main()
 
 
