138470368e420b2b863de1f4e50005088014efe3
github_username_here <email@example.com>
2025-03-24 09:38:49 -0400

    co ed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = numwords(text)
-    letter_count = numletters(text)
+    letter_count = numcharacters(text)
     print(f"{word_count} words found in the document")
     return letter_count
 main()
