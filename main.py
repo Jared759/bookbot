82a2f91c8ee299ae6a30aa36c5e3f65325e5ce47
github_username_here <email@example.com>
2025-03-24 10:51:58 -0400

    co ed

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def get_book_text(path):
         return file_contents
 
 def main():
-      if len(sys.argv) != 2:
-        print(f"Usage: python3 main.py books/")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     
-book_path = sys.argv(1)
+    book_path = sys.argv[1]
 text = get_book_text(book_path)
 word_count = numwords(text)
 letter_count = numcharacters(text)  # This should return the letter frequency dictionary
