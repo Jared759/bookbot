1dbad68bcf6e5326dfb62f6a5a19a2ee335f900d
github_username_here <email@example.com>
2025-03-24 10:57:07 -0400

    co ed

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ book_path = sys.argv[1]
 
 with open(book_path, "r") as f:
 
-text = get_book_text(book_path)
-word_count = numwords(text)
-letter_count = numcharacters(text)  # This should return the letter frequency dictionary
-sorted_letters = sort(letter_count)
-print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
-print("----------- Word Count ----------")
-print(f"Found {word_count} total words")
-print("--------- Character Count -------")
-for item in sorted_letters:  # Loop through the sorted list
-    print(f"{item['letter']}: {item['count']}")
+    text = get_book_text(book_path)
+    word_count = numwords(text)
+    letter_count = numcharacters(text)  # This should return the letter frequency dictionary
+    sorted_letters = sort(letter_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_letters:  # Loop through the sorted list
+        print(f"{item['letter']}: {item['count']}")
 
 main()
 
